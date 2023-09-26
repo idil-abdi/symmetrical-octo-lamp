1c601305af072aaed70f7d5a661550962bd52dd7
Edil Abdiaziz <edilabdiaziz@gmail.com>
2023-09-26 14:06:00 +0100

    slidshow

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+//slideshow
+let index = 0;
+slideshow();
+
+function slideshow() {
+    let i;
+    const image = document.getElementsByClassName('slides');
+    for (i = 0; i < image.length; i++) {
+        image[i].style.display = 'none';
+    }
+    index++;
+    if(index > image.length) {
+        index = 1
+    } 
+    image[index-1].style.display = 'block';
+    setTimeout(slideshow, 4000);
+}
